d615a8659eaf3c8c8b087deb3594528112cbb2b7
nju33 <nju33.ki@gmail.com>
2018-12-28 14:02:22 +0900

    fix: monorepo

---
+++ example/src/index.ts
@@ -0,0 +1,29 @@
+import vwxy, {VwxyValue} from 'vwxy';
+
+(() => {
+  const baz = vwxy<{
+    foo: {
+      bar: {
+        baz: VwxyValue<number>;
+      };
+    };
+  }>().foo.bar.baz(123);
+
+  console.log(
+    'vwxy().foo.bar.baz(123) ',
+    baz({
+      foo: {bar: {baz: 33}},
+    }),
+  );
+
+  const value = vwxy<{
+    arr: {
+      value: VwxyValue<string>;
+    }[];
+  }>().arr[1].value('bar');
+
+  console.log(
+    `vwxy().arr[1].value('baz') `,
+    value({arr: [{value: 'foo'}, {value: 'baz'}]}),
+  );
+})();
